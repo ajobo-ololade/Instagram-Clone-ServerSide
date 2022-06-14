1def15a8257ddda4a8d2997f4de2186e2b20a920
AJOBO OLOLADE <ajoboololade@gmail.com>
2022-06-14 15:10:15 +0100

    SERVER SIDE

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
-app.use(express.static(__dirname+'/build'))
+
 const app = express()
+app.use(express.static(__dirname+'/build'))
 const cors = require ("cors")
 
 const bodyParser = require("body-parser")
